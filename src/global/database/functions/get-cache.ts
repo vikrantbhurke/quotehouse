e806239ef32f402447e839e823cb41bd5b6ec4fc
Vikrant Bhurke <bhurkevi@gmail.com>
2025-11-06 21:53:53 +0530

    remove unnecessary redis db functions getCache

--- src/global/database/functions/get-cache.ts
+++
@@ -1,6 +0,0 @@
-import { connectRedis } from "@/global/configurations";
-
-export const getCache = async (key: string) => {
-  const redis = await connectRedis();
-  return await redis.get(key);
-};
