af35c4924ebd8e3967e255eb445c40255dcf8c76
Vikrant Bhurke <bhurkevi@gmail.com>
2025-11-06 21:50:40 +0530

    remove unnecessary redis db functions

--- src/global/database/functions/del-cache.ts
+++
@@ -1,6 +0,0 @@
-import { connectRedis } from "@/global/configurations";
-
-export const delCache = async (key: string) => {
-  const redis = await connectRedis();
-  await redis.del(key);
-};
--- src/global/database/functions/del-prefix-cache.ts
+++
@@ -1,7 +0,0 @@
-import { connectRedis } from "@/global/configurations";
-
-export const delPrefixCache = async (key: string) => {
-  const redis = await connectRedis();
-  const keys = await redis.keys(key);
-  keys.forEach(async (key) => await redis.del(key));
-};
