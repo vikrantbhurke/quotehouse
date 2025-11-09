f2653e2351dc15acd75f123b4b1781fb8f014072
Vikrant Bhurke <bhurkevi@gmail.com>
2025-11-09 22:42:51 +0530

    improve about page styles

--- src/app/(appshell)/about/page.tsx
+++ src/app/(appshell)/about/page.tsx
@@ -17,29 +17,35 @@ const text = clsx(outfit.className, "antialiased");
 
 export default function Page() {
   return (
-    <Stack gap={60} p="lg">
+    <Stack gap={60} p="xl">
       <Stack>
-        <Title ta="center" order={1} c="#333" fw={600} className={text}>
+        <Title ta="center" order={1} c="#333" fw={900} className={text}>
           Hello World
         </Title>
 
-        <Text c="#333" fz={24} fw={600} className={text}>
-          I&apos;m Vikrant. I&apos;m addicted to doom scroll social media
-          everyday just like you are. The content I consume is mostly mental
-          junk food. It&apos;s tasty, cheap and even safe in small dosage. But
-          it&apos;s unhealthy and unnecessary. I also and less frequently read
-          books, watch tutorials and listen to podcasts. That&apos;s a healthier
-          diet. Protein, complex carbs, fibre. I made this app to replace some
-          of the former with the later. Fries with fruits. Nachos with nuts.
-          Biscuits with berries. I am also manually and continually growing the
-          quotes database. You&apos;re welcome to help me too. I read, verify
-          and add qualified quotes to the database. You can submit quotes you
-          think should be on this app with the form below.
+        <Text c="#333" fz={20} fw={400} className={text}>
+          I&apos;m Vikrant. Addicted to doom scroll some app everyday just like
+          you are. The content I consume on those apps is mostly mental junk
+          food. It&apos;s tasty, cheap and even safe in small dosage. But
+          it&apos;s unhealthy and unnecessary. I also but less frequently read
+          books, watch tutorials and listen to podcasts. That&apos;s a lot
+          healthier diet. Protein, complex carbs, fibre. I made this app to
+          replace some of the former with the later. Fries with fruits. Nachos
+          with nuts. Biscuits with berries. I know there exist alternate
+          websites who have more quotes, have more ways to filter and search
+          them and add more quotes faster in their database. I&apos;m not
+          competing with them. I&apos;m simply built a site / app where I can
+          get my daily fix of insightful thoughts in easy and aesthetically
+          pleasing way. Something I couldn&apos;t find on the internet until I
+          made this app. I am also manually and continually growing the quotes
+          database. You&apos;re welcome to help me do that. You can submit
+          quotes you think should be on this app with the form below. I read,
+          verify and add qualified quotes to the database.
         </Text>
       </Stack>
 
       <Stack>
-        <Title ta="center" order={1} c="#333" fw={600} className={text}>
+        <Title ta="center" order={1} c="#333" fw={900} className={text}>
           Contact
         </Title>
 
@@ -55,9 +61,16 @@ export default function Page() {
           autosize
           minRows={2}
           maxRows={4}
-          styles={{ input: { border: "3px solid #EEE" } }}
+          styles={{
+            input: {
+              border: "3px solid #EEE",
+            },
+          }}
           c="#333"
           fw={600}
+          classNames={{
+            input: text,
+          }}
           className={text}
         />
 
@@ -73,6 +86,9 @@ export default function Page() {
           styles={{ input: { border: "3px solid #EEE" } }}
           c="#333"
           fw={600}
+          classNames={{
+            input: text,
+          }}
           className={text}
         />
 
@@ -88,14 +104,25 @@ export default function Page() {
             </Text>
           }
           classNames={{
-            dropdown: text,
+            option: text,
+            // dropdown: text,
+            input: text,
           }}
           styles={{
-            dropdown: { border: "3px solid #EEE" },
+            dropdown: {
+              border: "3px solid #EEE",
+              borderRadius: 20,
+            },
             input: { border: "3px solid #EEE" },
           }}
           placeholder="Pick value"
-          data={["React", "Angular", "Vue", "Svelte"]}
+          data={[
+            "Submit a quote",
+            "Report a bug",
+            "Make a suggestion",
+            "Ask a question",
+            "Request a feature",
+          ]}
         />
 
         <Space />
@@ -115,55 +142,71 @@ export default function Page() {
 export function QuotesTimeline() {
   return (
     <Stack pb="xl" align="center">
-      <Title ta="center" order={1} c="#333" fw={600} className={text}>
+      <Title ta="center" order={1} c="#333" fw={900} className={text}>
         Timeline
       </Title>
 
-      <Timeline active={1} bulletSize={50} lineWidth={5} className={text}>
+      <Timeline
+        active={1}
+        color="violet"
+        bulletSize={50}
+        lineWidth={10}
+        className={text}>
         <TimelineItem
-          bullet={"1K"}
+          bullet={
+            <Title order={5} className={text}>
+              1K
+            </Title>
+          }
           title={
             <Text c="#333" fz={24} fw={600} className={text}>
               Day 0: Launch
             </Text>
           }>
-          <Text c="#666" size="md" fw={600} className={text}>
+          <Text c="#888" size="md" fw={600} className={text}>
             Quotehouse website lauched with 1000 quotes
           </Text>
-          <Text c="#666" size="md" mt={4} fw={600}>
+          <Text c="#bbb" size="md" mt={4} fw={600}>
             10th November, 2025
           </Text>
         </TimelineItem>
 
         <TimelineItem
-          bullet={"10K"}
+          bullet={
+            <Title order={5} className={text}>
+              10K
+            </Title>
+          }
           title={
             <Text c="#333" fz={24} fw={600} className={text}>
-              Day 123: Launch
+              Day 123
             </Text>
           }>
-          <Text c="#666" size="md" fw={600} className={text}>
+          <Text c="#888" size="md" fw={600} className={text}>
             Quotehouse hit 10000 quotes in its datebase. Thanks to the global
             community.
           </Text>
-          <Text c="#666" size="md" mt={4} fw={600}>
+          <Text c="#bbb" size="md" mt={4} fw={600}>
             1st January, 2026
           </Text>
         </TimelineItem>
 
         <TimelineItem
-          bullet={"100K"}
+          bullet={
+            <Title order={5} className={text}>
+              100K
+            </Title>
+          }
           title={
             <Text c="#333" fz={24} fw={600} className={text}>
-              Day 478: Launch
+              Day ???
             </Text>
           }>
-          <Text c="#666" size="md" fw={600} className={text}>
-            Quotehouse hit 10000 quotes in its datebase. Thanks to the global
-            community.
+          <Text c="#888" size="md" fw={600} className={text}>
+            Help us reach this milestone sooner.
           </Text>
-          <Text c="#666" size="md" mt={4} fw={600}>
-            1st January, 2026
+          <Text c="#bbb" size="md" mt={4} fw={600}>
+            31st June, 2026
           </Text>
         </TimelineItem>
       </Timeline>
--- src/global/components/layout/header.tsx
+++ src/global/components/layout/header.tsx
@@ -1,11 +1,20 @@
+"use client";
 import { Group, ThemeIcon } from "@mantine/core";
 import { ButtonInstallApp } from "../common";
+import { useWindowScroll } from "@mantine/hooks";
+import clsx from "clsx";
 
-const group = `bg-white w-full max-w-3xl fixed top-0 z-100`;
+const group = `bg-white w-full max-w-3xl fixed top-0 z-100 border-b-[3px]`;
 
 export function Header() {
+  const [scroll] = useWindowScroll();
+
   return (
-    <Group h={60} p="xs" className={group} justify="space-between">
+    <Group
+      h={60}
+      p="xs"
+      className={clsx(group, scroll.y > 0 ? "border-gray-200" : "border-white")}
+      justify="space-between">
       <ThemeIcon bg="#333" size="lg" radius="xl" />
 
       <ButtonInstallApp />
