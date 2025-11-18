3f90b086593a9028bf1929e9def0cc93dfbfffd8
Vikrant Bhurke <bhurkevi@gmail.com>
2025-11-18 19:40:53 +0530

    quote citation alert

--- src/features/cited-quote/views/alert-quote-citation.tsx
+++ src/features/cited-quote/views/alert-quote-citation.tsx
@@ -1,44 +1,36 @@
 import { outfit } from "@/global/styles/fonts";
-import { Alert, Stack, Text, ThemeIcon, Title } from "@mantine/core";
-import { IconInfoCircle } from "@tabler/icons-react";
+import { Stack, Text, Title } from "@mantine/core";
 import clsx from "clsx";
 
 const text = clsx(outfit.className, "antialiased");
 
 export function AlterQuoteCitation() {
   return (
-    <Alert
-      radius="md"
-      color="gray"
-      variant="light"
-      title={
-        <Title order={4} className={text} c="black">
-          Points To Consider For Citing Quotes
-        </Title>
-      }
-      icon={
-        <ThemeIcon size={28} variant="transparent" c="black">
-          <IconInfoCircle size={28} />
-        </ThemeIcon>
-      }>
-      <Stack>
-        <Text fz={18}>
-          1) Quote already existing in our database, even the one that
-          don&apos;t match verbatim but roughly in terms of meaning will be
-          rejected.
-        </Text>
-        <Text fz={18}>
-          2) Quote should actually be meaningful, not simply something someone
-          famous said sometime.
-        </Text>
-        <Text fz={18}>
-          3) Quote length can be anything but generally shorter the better.
-        </Text>
-        <Text fz={18}>
-          4) Quote that is funny or poetic or rhythmic, but lacking some
-          inherent logical or practical meaning will be rejected.
-        </Text>
-      </Stack>
-    </Alert>
+    <Stack
+      p="xl"
+      style={{
+        backgroundColor: "var(--mantine-color-yellow-1)",
+        borderRadius: 20,
+      }}>
+      <Title order={4} className={text} c="black">
+        Points To Consider For Citing Quotes
+      </Title>
+
+      <Text fz={18}>
+        1) Quote already existing in our database, even the one that don&apos;t
+        match verbatim but roughly in terms of meaning, will be rejected.
+      </Text>
+      <Text fz={18}>
+        2) Quote should actually be meaningful, not simply something someone
+        famous said sometime.
+      </Text>
+      <Text fz={18}>
+        3) Quote length can be anything but generally shorter the better.
+      </Text>
+      <Text fz={18}>
+        4) Quote that is funny or poetic or rhythmic, but lacking some inherent
+        logical or practical meaning will be rejected.
+      </Text>
+    </Stack>
   );
 }
